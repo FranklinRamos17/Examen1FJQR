96d2b929601d2c401a505bf175ec2b63521950e6
FranklinRamos17 <103528477+FranklinRamos17@users.noreply.github.com>
2022-05-12 17:29:25 -0500

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -14,11 +14,11 @@ public class Ejercicio1 {
         tp=lt.nextDouble();
         promedio=ec*0.4+ep*0.25+tp*0.35;
         if (promedio>=17 && promedio<=20) {
-            estado="\n\nAprobo \n\tSe encuentra en el nivel 4 y alcansa una vacante";
+            estado="\n\nAprobo \n\tSe encuentra en el nivel 4 y alcanza una vacante";
         }else if(promedio<17 && promedio>=14){
-            estado="\n\nAprobo \n\tSe encuentra en el nivel 3 y alcansa una vacante";
+            estado="\n\nAprobo \n\tSe encuentra en el nivel 3 y alcanza una vacante";
         }else if(promedio<14 && promedio>=11){
-            estado="\n\nAprobo \n\tSe encuentra en el nivel 2 y alcansa una vacante";
+            estado="\n\nAprobo \n\tSe encuentra en el nivel 2 y alcanza una vacante";
         }else if(promedio<11){
             estado="\n\nDesaprobo y pierde la vacante \n\tSe encuentra en el nivel 1";
         }else{
